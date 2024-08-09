90757d4589e5bd9de924495e5ec287c2999570c0
yassineboucham <127396067+yassineboucham@users.noreply.github.com>
2024-08-09 23:56:30 +0100

    bmi

---
+++ JavaScript/bmi.js
@@ -0,0 +1,11 @@
+function bmiCalculator (weight, height) {
+    var bmi = weight / Math.pow(height,2);
+    if (bmi < 18.5){
+        return "Your BMI is "+bmi+", so you are underweight."
+    } if(bmi >= 18.5 && bmi <= 24.9){
+        return "Your BMI is "+bmi+", so you have a normal weight."
+    } else{
+        return "Your BMI is "+bmi+", so you are overweight."
+    }
+    return interpretation;
+}
