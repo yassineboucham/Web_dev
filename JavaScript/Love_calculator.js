bdcf508a552125f26c1a6e0bd5711329e3a4f94e
yassineboucham <127396067+yassineboucham@users.noreply.github.com>
2024-08-09 23:20:50 +0100

    love calc

---
+++ JavaScript/Love_calculator.js
@@ -0,0 +1,4 @@
+var persen1 = prompt("name of persen 1");
+var persen2 = prompt("name of persen 2");
+var love = (Math.random() * 100) + 1;
+console.log(persen1 + " & " + persen2 + ": " + Math.floor(love));
