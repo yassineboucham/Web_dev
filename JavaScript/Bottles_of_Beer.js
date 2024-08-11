5e7e81f5f5e6cd01a35bb8a23167a624c1f5aff5
yassineboucham <127396067+yassineboucham@users.noreply.github.com>
2024-08-11 10:38:23 +0100

    bottels

---
+++ JavaScript/Bottles_of_Beer.js
@@ -0,0 +1,10 @@
+var bottel = 99;
+while (bottel >= 2){
+    console.log(bottel + "bottles of beer on the wall," + bottel + " bottles of beer.");
+    console.log("Take one down and pass it around," + bottel - 1 + "bottles of beer on the wall.");
+    bottel--
+}
+console.log("1 bottle of beer on the wall, 1 bottle of beer.");
+console.log("Take one down and pass it around, no more bottles of beer on the wall.");
+console.log("No more bottles of beer on the wall, no more bottles of beer.");
+console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
