17552b7d5f27bd8ea8c93e4620367dfc60708faa
yassineboucham <yassineboucham11@email.com>
2025-01-22 15:33:10 +0100

    i will do it agin but step by step folling the logic of angila

--- JQUERY/simon_gaim/script.js
+++ JQUERY/simon_gaim/script.js
@@ -1,53 +1,61 @@
 $("body").keypress(function(){
     $("h1").text("Level 1");
-let arr = [];
-let Level = 1;
-function simon_random_klik(){
-    let rand = Math.floor((Math.random() * 4) + 1)
-    arr.push(rand)
-    Level++;
-}
-function style_click(check){
-    if(check == 1 || check == 0){
-        $("#green").click(function(){
-            $(this).animate({opacity: '0.7'}).animate({opacity: '1'});
-        });
+    let arr = [];
+    let Level = 1;
+    function simon_random_klik(){
+        let rand = Math.floor((Math.random() * 4) + 1)
+        arr.push(rand)
+        Level++;
     }
-    if(check == 2 || check == 0){
-        $("#red").click(function(){
-            $(this).animate({opacity: '0.7'}).animate({opacity: '1'});
-        });
+    function click_random(nb){
+        if(nb == 1){
+            $("#green").animate({opacity: '0.4'}).animate({opacity: '1'});
+        }
+        if(nb == 2){
+            $("#red").animate({opacity: '0.4'}).animate({opacity: '1'});
+        }
+        if(nb == 3){
+            $("#yellow").animate({opacity: '0.4'}).animate({opacity: '1'});
+        }
+        if(nb == 4){
+            $("#blue").animate({opacity: '0.4'}).animate({opacity: '1'});
+        }
     }
-    if(check == 3 || check == 0){
-        $("#yellow").click(function(){
-            $(this).animate({opacity: '0.7'}).animate({opacity: '1'});
-        });
-    }
-    if(check == 4 || check == 0){
-        $("#blue").click(function(){
-            $(this).animate({opacity: '0.7'}).animate({opacity: '1'});
-        });
-    }
-}
-style_click(0);
-simon_random_klik();
-let arr_click = [];
-console.log(arr);
-$("div").click(function(){
-    arr_click.push(this.className);
-    if(arr[arr_click.length - 1] != arr_click[arr_click.length - 1]){
-        console.log("Gami over");
-        arr_click = [];
-        arr = [];
-        return;
-    }
-    $("h1").text("Level " + Level);
-    if(arr_click.length == arr.length){
-        arr_click = [];
-        console.log("level:" + Level);
-        simon_random_klik();
-    }
+    $("#green").click(function(){
+        $(this).animate({opacity: '0.7'}).animate({opacity: '1'});
+    });
+    $("#red").click(function(){
+        $(this).animate({opacity: '0.7'}).animate({opacity: '1'});
+    });
+    $("#yellow").click(function(){
+        $(this).animate({opacity: '0.7'}).animate({opacity: '1'});
+    });
+    $("#blue").click(function(){
+        $(this).animate({opacity: '0.7'}).animate({opacity: '1'});
+    });
+    simon_random_klik();
+    let arr_click = [];
     console.log(arr);
-    console.log(arr_click);
-});
+    $("div").click(function(){
+        $("h1").text("Level " + Level);
+        arr_click.push(this.className);
+        if(arr[arr_click.length - 1] != arr_click[arr_click.length - 1]){
+            console.log("Gami over");
+            $("h1").text("Gami over");
+            arr_click = [];
+            arr = [];
+            return;
+        }
+        if(arr_click.length == arr.length){
+            arr_click = [];
+            console.log("level:" + Level);
+            simon_random_klik();
+            for(let i = 0; i < arr.length; i++){
+                console.log(arr[i]);
+                click_random(arr[i]);
+            }
+        }
+        console.log(arr);
+        console.log(arr_click);
+    });
 });
