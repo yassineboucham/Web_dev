7b1b6630a13885b827c8af9b32c3c2155e7fd7e0
yassineboucham <yassineboucham11@email.com>
2025-01-22 16:09:27 +0100

    Show the Sequence to the User with Animations and Sounds

--- JQUERY/simon_gaim/Simon_Game/game.js
+++ JQUERY/simon_gaim/Simon_Game/game.js
@@ -4,4 +4,8 @@ function nextSequence(){
     let randomNumber = Math.floor((Math.random()*4));
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
+    audio.play();
 }
+nextSequence();
