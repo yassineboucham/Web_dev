3ecb67324c3bd21eef07ff422814cc5616d61a6a
yassineboucham <yassineboucham11@email.com>
2025-01-22 15:52:07 +0100

    Create A New Pattern

--- JQUERY/simon_gaim/Simon_Game/game.js
+++ JQUERY/simon_gaim/Simon_Game/game.js
@@ -1,1 +1,7 @@
-alert("hello world")
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+function nextSequence(){
+    let randomNumber = Math.floor((Math.random()*4));
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
