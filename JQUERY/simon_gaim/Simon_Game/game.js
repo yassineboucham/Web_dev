11374acaa540e9681bec8d059ee79e2502069e9a
yassineboucham <yassineboucham11@email.com>
2025-01-22 16:43:22 +0100

    Add Animations to User Clicks

--- JQUERY/simon_gaim/Simon_Game/game.js
+++ JQUERY/simon_gaim/Simon_Game/game.js
@@ -1,16 +1,28 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let userClickedPattern = [];
 let gamePattern = [];
+function playSound(name){
+    var audio = new Audio('sounds/' + name + '.mp3');
+    audio.play();
+}
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    }, 100);
+    
+}
 function nextSequence(){
-    let randomNumber = Math.floor((Math.random()*4));
+    let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeOut(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
-    audio.play();
+    playSound(randomChosenColour);
 }
 $(".btn").click(function(){
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 nextSequence();
