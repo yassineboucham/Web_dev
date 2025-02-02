8661f8736bc12892ab4d4cd50253a2ef91c1c472
yassineboucham <yassineboucham2002@gmail.com>
2025-02-02 13:03:31 +0100

    handle / /contact and /about endpoints.

--- backend/Express/3.2 HTTP Requests/index.js
+++ backend/Express/3.2 HTTP Requests/index.js
@@ -2,6 +2,18 @@ import express from "express"
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("<h1> Homme Page</h1>");
+});
+
+app.get("/About", (req, res) => {
+  res.send("<h1> About Page</h1>");
+});
+
+app.get("/Contact", (req, res) => {
+  res.send("<h1> Contact Page</h1>");
+});
+
 app.listen(port, () => {
   console.log("server raning on the port number " + port);
 });
