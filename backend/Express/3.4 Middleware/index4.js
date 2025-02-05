392a1617629922be4148d625e8648ce9af192249
yassineboucham <yassineboucham2002@gmail.com>
2025-02-05 13:34:09 +0100

    exercice done

--- backend/Express/3.4 Middleware/index4.js
+++ backend/Express/3.4 Middleware/index4.js
@@ -1,8 +1,32 @@
-import express from "express";
+  import express from "express";
+  import { dirname } from "path";
+  import { fileURLToPath } from "url";
+  import bodyParser from "body-parser";
+  const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express();
-const port = 3000;
+  const app = express();
+  const port = 3000;
+  var bandname = "";
+  
+  app.use(bodyParser.urlencoded({extended: true}));
+  
+  function band(req, res, next){
+    console.log(req.body);
+    bandname = req.body[" "] + req.body["pet"];
+    next();
+  }
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  app.use(band);
+  
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+  });
+
+  app.post("/submit", (req, res) => {
+    res.send("<h1>Your band is :</h1>" + bandname);
+  })
+
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
