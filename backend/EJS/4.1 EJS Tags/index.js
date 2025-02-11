230a0b190dddb2c58e2a18e53299ff2d04ce41e1
yassineboucham <yassineboucham2002@gmail.com>
2025-02-11 13:36:57 +0100

    done

--- backend/EJS/4.1 EJS Tags/index.js
+++ backend/EJS/4.1 EJS Tags/index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
     title: "EJS Tags",
     seconds: new Date().getSeconds(),
     items: ["apple", "banana", "cherry"],
-    htmlContent: "<strong>This is some strong text</strong>",
+    htmlContent: "<em>This is some strong text</em>",
   };
   res.render("index.ejs", data);
 });
