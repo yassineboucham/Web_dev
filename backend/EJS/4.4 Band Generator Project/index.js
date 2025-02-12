4a24a62201c0e99fca0cdc568785d89dbf9b431f
yassineboucham <yassineboucham2002@gmail.com>
2025-02-12 20:24:15 +0100

    fornt end done

--- backend/EJS/4.4 Band Generator Project/index.js
+++ backend/EJS/4.4 Band Generator Project/index.js
@@ -6,17 +6,19 @@ const port = 3000;
 
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
-app.use(express.static("public"));
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
+app.use(express.static("public"));
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
+const year = new Date().getFullYear();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs", {year : year});
 });
 
 app.post("/submit", (req, res) => {
--- backend/EJS/4.4 Band Generator Project/solution.js
+++ backend/EJS/4.4 Band Generator Project/solution.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
