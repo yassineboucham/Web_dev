d6e3af6af50b459c961d2c26fe82f9171ba86376
yassineboucham <yassineboucham2002@gmail.com>
2025-02-12 13:48:02 +0100

    add the footer and heder

--- backend/EJS/4.4 Band Generator Project/index.js
+++ backend/EJS/4.4 Band Generator Project/index.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
+app.use(express.static("public"));
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
 
