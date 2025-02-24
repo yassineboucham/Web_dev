261bbe0a3e7c790b6571d75c4af1959a49713bb4
yassineboucham <yassineboucham2002@gmail.com>
2025-02-24 20:30:29 +0000

    new dairectery

---
+++ backend/Node/2.3 NPM/index.js
@@ -0,0 +1,6 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+// console.log("Hello " + sillyName);
+import superheroes from 'superheroes';
+var name = superheroes.random();
+console.log(`I am ${name}!`);
