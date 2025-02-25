43a8df97ed5b6baa747a1db9e1b5736386df83bd
yassineboucham <yassineboucham2002@gmail.com>
2025-02-25 12:21:41 +0000

    all

--- backend/2.3 NPM/index.js
+++
@@ -1,6 +0,0 @@
-// var generateName = require('sillyname');
-// var sillyName = generateName();
-// console.log("Hello " + sillyName);
-import superheroes from 'superheroes';
-var name = superheroes.random();
-console.log(`I am ${name}!`);
