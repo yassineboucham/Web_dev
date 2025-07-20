a0a35a22a1bc234b715602170ef29fa9661c18b9
Yyaassiinnee2@@2 <yassineboucham2002@gmail.com>
2025-07-20 20:35:42 +0100

    d

---
+++ React/first_code/src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import List from './list.jsx';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <List />
+        </div>
+    )
+}
+
+export default App;
+
+
